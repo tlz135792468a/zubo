b8e84ba3fa81f14b3c971f19c1887f3c787f3642
tlz135792468a <1763860413@qq.com>
2026-04-05 14:42:02 +0800

    Update fofa_fetch.py

--- fofa_fetch.py
+++ fofa_fetch.py
@@ -19,7 +19,7 @@ COUNTER_FILE = "计数.txt"
 IP_DIR = "ip"
 RTP_DIR = "rtp"
 ZUBO_FILE = "zubo.txt"
-IPTV_FILE = "IPTV.txt"
+IPTV_FILE = "IPvv.txt"
 
 # ===============================
 # 分类与映射配置
@@ -477,7 +477,7 @@ def push_all_files():
     os.system("git add 计数.txt || true")
     os.system("git add ip/*.txt || true")
     os.system("git add IPTV.txt || true")
-    os.system('git commit -m "自动更新：计数、IP文件、IPV.txt" || echo "⚠️ 无需提交"')
+    os.system('git commit -m "自动更新：计数、IP文件、IPTV.txt" || echo "⚠️ 无需提交"')
     os.system("git push origin main || echo '⚠️ 推送失败'")
 
 # ===============================
